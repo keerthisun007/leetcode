f16b1cdbe7b2b03ee0823b153258a024c6ee61f4
keerthisun007 <keerthisun07@gmail.com>
2026-07-07 19:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (23.89%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -8,3 +8,11 @@ class Solution {
         return ans;
     }
 }
+/*
+1.return new double[]{kelvin, fahrenheit};
+2.(cel+9/5+32)is the formula
+3.double[] ans = new double[2]; //another method
+        ans[0] = kelvin; 
+        ans[1] = fahrenheit; 
+        return ans;
+*/
