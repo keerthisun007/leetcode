26429920a438b0cd692d858cefc11c079ee1a5fa
keerthisun007 <keerthisun07@gmail.com>
2026-07-10 15:26:45 +0530

    Time: 1 ms (99.86%) | Memory: 47.1 MB (11.68%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-       List<String> ans = new LinkedList<>();
-        
-        // Loop from 1 to n
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                ans.add("FizzBuzz");
-            } 
-            else if (i % 3 == 0) {
-                ans.add("Fizz");
-            } 
-            else if (i % 5 == 0) {
-                ans.add("Buzz");
-            } 
-            else {
-                ans.add(String.valueOf(i));
-            }
+       List ans=new ArrayList();
+       for(int i=1;i<=n;i++){
+        if(i%3==0 && i%5==0){
+            ans.add("FizzBuzz");
         }
-        
-        return ans;
+     else if(i%3==0){
+        ans.add("Fizz");
+     }
+     else if(i%5==0){
+        ans.add("Buzz");
+     }
+     else{
+        ans.add(String.valueOf(i));
+     }
+       }
+       return ans;
     }
 }
