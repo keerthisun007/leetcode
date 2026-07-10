ca3976e350fe1b1cc73f08b553b072b0c7d83998
keerthisun007 <keerthisun07@gmail.com>
2026-07-10 15:38:33 +0530

    Time: 1 ms (99.86%) | Memory: 47 MB (27.57%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -18,3 +18,29 @@ class Solution {
        return ans;
     }
 }
+/*
+ArrayList vs LinkedList:
+We used ArrayList because it is faster and more memory-efficient when simply appending elements to the end of a list sequentially.
+same program using array:
+String[] ans = new String[n];
+        
+        // Loop from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Array index starts at 0, so we map number 'i' to index 'i - 1'
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans[i - 1] = "FizzBuzz";
+            } 
+            else if (i % 3 == 0) {
+                ans[i - 1] = "Fizz";
+            } 
+            else if (i % 5 == 0) {
+                ans[i - 1] = "Buzz";
+            } 
+            else {
+                ans[i - 1] = String.valueOf(i);
+            }
+        }
+        
+        return ans;
+    }
+    */
