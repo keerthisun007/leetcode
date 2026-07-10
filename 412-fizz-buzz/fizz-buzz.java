a106707fcc84b09b73924014b091bfe5ea15326c
keerthisun007 <keerthisun07@gmail.com>
2026-07-10 15:14:05 +0530

    Time: 1 ms (99.86%) | Memory: 46.7 MB (83.14%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -2,21 +2,18 @@ class Solution {
     public List<String> fizzBuzz(int n) {
        List<String> ans=new ArrayList<>();
        for(int i=1;i<=n;i++){
-       if (i % 3 == 0 && i % 5 == 0) {
-                ans.add("FizzBuzz");
-            } 
-            // 2. Check divisibility by 3
-            else if (i % 3 == 0) {
-                ans.add("Fizz");
-            } 
-            // 3. Check divisibility by 5
-            else if (i % 5 == 0) {
-                ans.add("Buzz");
-            } 
-            // 4. If none match, add the number itself as a string
-            else {
-                ans.add(String.valueOf(i));
-            }
+        if(i%3==0 && i%5==0){
+            ans.add("FizzBuzz");
+        }
+     else if(i%3==0){
+        ans.add("Fizz");
+     }
+     else if(i%5==0){
+        ans.add("Buzz");
+     }
+     else{
+        ans.add(String.valueOf(i));
+     }
        }
        return ans;
     }
