8f3b965cb612b9dc024a03b34cb6028b552fe0ad
keerthisun007 <keerthisun07@gmail.com>
2026-08-04 22:37:08 +0530

    Time: 1 ms (99.95%) | Memory: 46.8 MB (65.80%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -7,3 +7,30 @@ class Solution {
         return result;
     }
 }
+//If duplicate numbers should cancel out and only the unique number should remain, use XOR (^).
+
+/*
+check each element with the entire list
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
+*/
