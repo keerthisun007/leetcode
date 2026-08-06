31044e614fc6745535740331086d98c56d371165
keerthisun007 <keerthisun07@gmail.com>
2026-08-06 21:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (53.14%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,23 +1,40 @@
 class Solution {
     public int fib(int n) {
-       if (n == 0)
-            return 0;
 
-        if (n == 1)
-            return 1;
-        int a=0;
-        int b=1;
-        int c=0;
-        int i=1;
-        while(i<n){
-            c=a+b;
-            a=b;
-            b=c;
-            i++;
+        int first = 0;
+        int second = 1;
+
+        for(int i = 1; i <= n; i++){
+
+            int third = first + second;
+
+            first = second;
+            second = third;
         }
-        return b;
+
+        return first;
     }
 }
+// class Solution {
+//     public int fib(int n) {
+//        if (n == 0)
+//             return 0;
+
+//         if (n == 1)
+//             return 1;
+//         int a=0;
+//         int b=1;
+//         int c=0;
+//         int i=1;
+//         while(i<n){
+//             c=a+b;
+//             a=b;
+//             b=c;
+//             i++;
+//         }
+//         return b;
+//     }
+// }
 
 /*
 Question: Print Fibonacci Series → System.out.print(a + " ");
