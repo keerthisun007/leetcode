6266cfd6816475d45f8b8b084849d1d6d623674f
keerthisun007 <keerthisun07@gmail.com>
2026-08-06 22:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -5,21 +5,22 @@ class Solution {
 
         if (n == 1)
             return 1;
-        // int a=0;
-        // int b=1;
-        // int c=0;
-        // int i=1;
-        // while(i<n){
-        //     c=a+b;
-        //     a=b;
-        //     b=c;
-        //     i++;
-        // }
-        return fib(n-1)+fib(n-2);
+        int a=0;
+        int b=1;
+        int c=0;
+        int i=1;
+        while(i<n){
+            c=a+b;
+            a=b;
+            b=c;
+            i++;
+        }
+        return c;
     }
 }
 
 /*
 Question: Print Fibonacci Series → System.out.print(a + " ");
 Question: Print nth Fibonacci Number → System.out.println(b);
+we can also use the recursion concept also that is return fib(n-1)+fib(n-2) but the base condition is important
 */
