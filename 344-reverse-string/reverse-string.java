18fcdc491631ed774408bf722c4c5dbac0ff44ed
keerthisun007 <keerthisun07@gmail.com>
2026-08-09 20:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (38.37%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,12 +1,32 @@
 class Solution {
     public void reverseString(char[] s) {
-       char[] reversed = new char[s.length];
-        for (int i = 0; i < s.length; i++) {
-            reversed[i] = s[s.length - 1 - i];
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
         }
-      for(int i=0;i<s.length;i++){
-        s[i]=reversed[i];
-      }
     }
 }
+
+
+
+
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//        char[] reversed = new char[s.length];
+//         for (int i = 0; i < s.length; i++) {
+//             reversed[i] = s[s.length - 1 - i];
+//         }
+//       for(int i=0;i<s.length;i++){
+//         s[i]=reversed[i];
+//       }
+//     }
+// }
 //you need to remember the length function and this logic reversed[i] = s[s.length - 1 - i];
