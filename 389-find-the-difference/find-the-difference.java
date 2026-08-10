0a6ebe15aa9fa038a1fdc4130b32512f5ec1945f
keerthisun007 <keerthisun07@gmail.com>
2026-08-10 21:38:26 +0530

    Time: 2 ms (66.31%) | Memory: 43.2 MB (55.00%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -10,3 +10,11 @@ class Solution {
 return keerthi;
     }
 }
+/*
+First loop: Take all characters from s and store them in ans using XOR; second loop: take all characters from t and XOR them with the same ans.
+Since the same characters occur in both strings, they cancel each other, leaving only the one extra character in t as the answer.
+
+XOR rule: A ^ A = 0, A ^ 0 = A — so duplicate values cancel each other.
+Use XOR when: answer = a ^ b ^ a → b remains; useful when every element appears twice except one.
+
+*/
