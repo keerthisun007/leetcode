caa3a2cf3ee1de9f8805bee900c6d743d895222a
keerthisun007 <keerthisun07@gmail.com>
2026-08-12 21:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (35.88%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -5,3 +5,12 @@ class Solution {
        }
        return nums;
     }}
+
+    /*
+    Start from i = 1 because nums[0] is already the first running sum, then add each number to the previous running sum.
+nums[i] = nums[i] + nums[i-1] updates the array with the cumulative sum and finally returns it.
+
+nums[i] = nums[i] + nums[i-1] adds the current number to the previous running sum; importantly, nums[i-1] has already been changed in the previous loop, so it contains the updated sum (e.g., nums[1] changes from 2 to 3).
+Thus each step uses the new value stored at the previous index to build the next sum: [1,2,3,4] → [1,3,6,10]
+
+*/
