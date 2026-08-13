da0b8e696f81b65ee394c4c39480d8b775d16354
keerthisun007 <keerthisun07@gmail.com>
2026-08-13 07:15:46 +0530

    Time: 5 ms (61.75%) | Memory: 44.1 MB (90.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -18,3 +18,7 @@ class Solution {
         return true;
     }
 }
+/*
+count stores how many times each letter appears: add +1 for characters in s and -1 for characters in t; if all counts become 0, they are anagrams. ✅
+charAt(i) - 'a' converts a letter into an array index (a→0, b→1, ..., z→25) so we know which letter’s count to update.
+*/
