ea8d8e95b3f74c5ec6d7f4f11501389a63bdf8eb
keerthisun007 <keerthisun07@gmail.com>
2026-08-15 19:38:09 +0530

    Time: 2 ms (93.84%) | Memory: 64.9 MB (87.02%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-
         int n = nums.length;
-
         int[] left = new int[n];
         int[] right = new int[n];
         int[] answer = new int[n];
-
-        // Create LEFT array
         int product = 1;
-
         for (int i = 0; i < n; i++) {
             left[i] = product;
             product = product * nums[i];
         }
-
-        // Create RIGHT array
-        product = 1;
-
+            product = 1;
         for (int i = n - 1; i >= 0; i--) {
             right[i] = product;
             product = product * nums[i];
         }
-
-        // Multiply LEFT × RIGHT
         for (int i = 0; i < n; i++) {
             answer[i] = left[i] * right[i];
         }
-
         return answer;
     }
 }
+/*
+First loop: Store the product of all numbers before i in left, and second loop stores the product of all numbers after i in right.
+For each position, left[i] × right[i] gives the product of every number except the current number nums[i].
+Example: [1,2,3,4] → left[2]=2, right[2]=4 → 2×4=8, so 3 is automatically skipped.
+*/
