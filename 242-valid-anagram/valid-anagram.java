026c4937bb2bb9779c81879b5ac3c0da225dcb1f
keerthisun007 <keerthisun07@gmail.com>
2026-08-15 20:09:28 +0530

    Time: 6 ms (43.18%) | Memory: 44.3 MB (75.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,7 +5,7 @@ class Solution {
         int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
+            count[s.charAt(i) - 97]++;
             count[t.charAt(i) - 'a']--;
         }
 
