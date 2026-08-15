6f6f4d354eb2914bebaa21cade6115e43dffbbaf
keerthisun007 <keerthisun07@gmail.com>
2026-08-15 18:53:16 +0530

    Time: 1 ms (99.85%) | Memory: 71.9 MB (53.91%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] keerthi = new int[nums.length];
+
+        // LEFT PRODUCTS
+        int product = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            keerthi[i] = product;
+            product = product * nums[i];
+        }
+
+        // RIGHT PRODUCTS
+        product = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            keerthi[i] = keerthi[i] * product;
+            product = product * nums[i];
+        }
+
+        return keerthi;
+    }
+}
