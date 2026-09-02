89f97c2023e5f785617c6c0038e2668adbac235c
keerthisun007 <keerthisun07@gmail.com>
2026-09-02 20:27:59 +0530

    Time: 8 ms (4.84%) | Memory: 44.5 MB (5.53%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverseBits(int n) {
+        String a = Integer.toBinaryString(n);
+        
+        // Fix 1: Pad with leading zeros so length is always 32
+        while (a.length() < 32) {
+            a = "0" + a;
+        }
+        
+        // Your logic: Reverse the string
+        String b = "";
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+        
+        // Fix 2: Parse as unsigned integer to prevent overflow
+        return Integer.parseUnsignedInt(b, 2);
+    }
+}
