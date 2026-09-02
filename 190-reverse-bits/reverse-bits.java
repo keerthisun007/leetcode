7518a414b6b5a09d037ce5e86709a46a4071ab0d
keerthisun007 <keerthisun07@gmail.com>
2026-09-02 20:58:54 +0530

    Time: 8 ms (4.84%) | Memory: 43.9 MB (5.53%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,18 +2,18 @@ class Solution {
     public int reverseBits(int n) {
         String a = Integer.toBinaryString(n);
         
-        // Fix 1: Pad with leading zeros so length is always 32
+        //Integer.toBinaryString(n) munnadi irukkura zeros-a cut panradhaala, 32 bits-um reverse aagama thappana answer varudhu.
+
+// Correct answer vara, reverse pandradhukku munnadi string length 32 aagura varaikkum front-la "0" add pannanum.
         while (a.length() < 32) {
             a = "0" + a;
         }
         
-        // Your logic: Reverse the string
         String b = "";
         for (int i = a.length() - 1; i >= 0; i--) {
             b += a.charAt(i);
         }
         
-        // Fix 2: Parse as unsigned integer to prevent overflow
         return Integer.parseInt(b, 2);
     }
 }
