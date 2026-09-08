b65f7304fe7863139b427b6f36cecc6e6828f069
keerthisun007 <keerthisun07@gmail.com>
2026-09-08 23:31:56 +0530

    Time: 8 ms (92.62%) | Memory: 46 MB (66.03%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -4,5 +4,6 @@ class Solution {
         while(n%3==0){
             n/=3;
         }
-     return n == 1;
+     return n == 1;//understand the difference between return n==1 and return true..
     }}
+    //The loop keeps dividing n by 3 while it is divisible by 3. If n finally becomes 1, it is a power of 3; otherwise, it isn't.
