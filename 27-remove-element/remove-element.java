e71b87f9ef1206b0908aa0a807b2f97dc2d37466
keerthisun007 <keerthisun07@gmail.com>
2026-09-11 23:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (23.78%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -10,3 +10,4 @@ class Solution {
             return count;
     }
 }
+//i reads every element, and count marks where the next element not equal to val should be placed.If nums[i] == val, continue skips it; otherwise, we copy it to nums[count] and increase count.return count returns the number of valid elements, and LeetCode only checks the first count positions of the modified array.
