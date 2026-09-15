4480bcf4e9643deb5bbe9e0446900598d12ade93
keerthisun007 <keerthisun07@gmail.com>
2026-09-15 18:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (41.94%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -5,3 +5,4 @@ class Solution {
        return sasu - keerthi ;
     }
 }
+//(high + 1) / 2 counts odd numbers from 0 to high, while low / 2 counts the odd numbers before low. Subtracting them gives the number of odd numbers in the range [low, high]
